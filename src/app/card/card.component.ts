98ae46b6fd6274a7e3bd4d56c4b26dd62f1aa0fc
rcrochepeyre <remi.crochepeyre@gmail.com>
2021-08-02 11:00:22 +0200

    Component card ajouté

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+    titleCard:string = 'Montant total des ventes';
+    montantVentes = Math.floor(Math.random() * (10000 - 1500 + 1)) + 1500;
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
